e0b03ab13463c7d055ef65df0222e800a90f9c3e
pbmacintyre <pbmacintyre@gmail.com>
2025-01-29 14:55:54 -0400

    Updating code base.

--- index.php
+++ index.php
@@ -1,33 +1,52 @@
-<?php
 
-require_once("ringcentral-php-functions.inc");
-show_errors();
+<!--<script src="https://apps.ringcentral.com/integration/ringcentral-embeddable/latest/adapter.js"></script>-->
+
+<?php
 
 require('includes/vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ )->load();
 
 $client_id = $_ENV['RC_APP_CLIENT_ID'];
-$client_secret = $_ENV['RC_APP_CLIENT_SECRET'];
-
-$ringcentral_embed_width	= "500" ;
-$ringcentral_embed_height	= "750" ;
 
 $server_URL = "https://apps.ringcentral.com/integration/ringcentral-embeddable/latest/app.html?" ;
 $server_URL .= "clientId=$client_id&appServer=https://platform.ringcentral.com" ;
 
-$server_URL .= "&disableGlip=false";
+// Add on Team Messaging feature
+// $server_URL .= "&disableGlip=false";
 
-//$server_URL .= "&disableGlip=false&enableSMSTemplate=1";
-//  $server_URL .= "&disableGlip=false&disableCall=true&disableMessages=true";
+// add on SMS Template feature
+// $server_URL .= "&enableSMSTemplate=1";
+
+// Turn off the Call placement (dialpad) feature
+//  $server_URL .= "&disableCall=true";
+
+// Turn off the Call history feature
+//  $server_URL .= "&disableCallHistory=true";
+
+// Turn off the Contacts feature
+// $server_URL .= "&disableContacts=true";
+
+// Turn off the Messages feature
+//$server_URL .= "&disableMessages=true";
+
+// Turn off the Video meetings feature
+//$server_URL .= "&disableMeeting=true";
+
+
+$iframe_width	= "500" ;
+$iframe_height	= "750" ;
 
 $iframe_string = "<iframe id='rc-widget' allow='microphone' ";
-$iframe_string .= "width='$ringcentral_embed_width' ";
-$iframe_string .= "height='$ringcentral_embed_height' ";
+$iframe_string .= "width='$iframe_width' ";
+$iframe_string .= "height='$iframe_height' ";
 $iframe_string .= "src='$server_URL'";
 $iframe_string .= " ></iframe>";
+ ?>
 
-page_header(); ?>
-<!--<img src="images/rc-logo.png"/>-->
-<?php echo "<br/>" . $iframe_string ;
-page_footer();
+<!DOCTYPE html><html class='SiteWide'><head >
+<title >Embeddable Deep Dive App</title>
+<link rel='stylesheet' href='css/ringcentral-custom.css'></head>
 
+<?php echo "<br/>" . $iframe_string ; ?>
+
+</html>
